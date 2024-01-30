6de12ffdd42a21992190b4fefefd91a52671021a
amitkasabe <amit.kasabe25@gmail.com>
2024-01-30 15:39:17 +0530

    test

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <section id="nav">
+        <nav id="nav-bar">
+            <ul class="nav-items">
+                <li class="items"> TravelPlanet </li>
+                <li class="items"> Browse Hotels </li>
+            </ul>
+            <li class="items"> Amit </li>
+        </nav>
+    </section>
+    <section id="tabled">
+        <div class="data-container">
+            <table id="table">
+                <thead>
+                    <td> Hotel </td>
+                    <td> Arrival </td>
+                    <td> Departure </td>
+                    <td> Type </td>
+                    <td> Guests </td>
+                    <td> Price </td>
+                    <td colspan="2"> Manage </td>
+                </thead>
+                <hr>
+                <tbody>
+                    <tr>
+                        <td> Marriott </td>
+                        <td> 2019-08-28 </td>
+                        <td> 2019-08-28 </td>
+                        <td> Luxary Site </td>
+                        <td> 7 </td>
+                        <td> Rs.980 </td>
+                        <td class="edit-btn"> Edit </td>
+                        <td class="Delete"> Delete </td>
+                    </tr>
+                    
+                    <tr>
+                        <td> Marriott </td>
+                        <td> 2019-08-28 </td>
+                        <td> 2019-08-28 </td>
+                        <td> Luxary Site </td>
+                        <td> 7 </td>
+                        <td> Rs.980 </td>
+                        <td class="edit-btn"> Edit </td>
+                        <td class="Delete"> Delete </td>
+                    </tr>
+                    
+                    <tr>
+                        <td> Marriott </td>
+                        <td> 2019-08-28 </td>
+                        <td> 2019-08-28 </td>
+                        <td> Luxary Site </td>
+                        <td> 7 </td>
+                        <td> Rs.980 </td>
+                        <td class="edit-btn"> Edit </td>
+                        <td class="Delete"> Delete </td>
+                    </tr>
+                    <tr>
+                        <td> Marriott </td>
+                        <td> 2019-08-28 </td>
+                        <td> 2019-08-28 </td>
+                        <td> Luxary Site </td>
+                        <td> 7 </td>
+                        <td> Rs.980 </td>
+                        <td class="edit-btn"> Edit </td>
+                        <td class="Delete"> Delete </td>
+                    </tr>
+                    <tr>
+                        <td> Marriott </td>
+                        <td> 2019-08-28 </td>
+                        <td> 2019-08-28 </td>
+                        <td> Luxary Site </td>
+                        <td> 7 </td>
+                        <td> Rs.980 </td>
+                        <td class="edit-btn"> Edit </td>
+                        <td class="Delete"> Delete </td>
+                    </tr>
+                    <tr>
+                        <td> Marriott </td>
+                        <td> 2019-08-28 </td>
+                        <td> 2019-08-28 </td>
+                        <td> Luxary Site </td>
+                        <td> 7 </td>
+                        <td> Rs.980 </td>
+                        <td class="edit-btn"> Edit </td>
+                        <td class="Delete"> Delete </td>
+                    </tr>
+                    
+                </tbody>
+
+            </table>
+        </div>
+    </section>
+</body>
+</html>
